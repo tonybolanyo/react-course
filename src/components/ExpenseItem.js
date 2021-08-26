c310155c658fd7bd34ddcbffa4aca65264292703
Tony G. Bolaño <tonybolanyo@gmail.com>
2021-08-26 19:13:36 +0200

    feat: props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
 import "./ExpenseItem.css"
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+
   return (
     <div className="expense-item">
-      <div>28 marzo 2021</div>
+      <div>{props.date.toLocaleDateString()}</div>
       <div className="expense-item__description">
-        <h2>Samsung Tablet</h2>
-        <div className="expense-item__price">203.25€</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}€</div>
       </div>
     </div>
   )
